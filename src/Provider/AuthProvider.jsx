b534d4c3d8d7ac721417cfdea6e9ee1e34e7d685
sajjad <sajjadsajjad098765@gmail.com>
2024-08-19 22:31:30 +0600

    authprovider and react-dom setup complete

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,55 @@
+import { createContext, useEffect, useState } from "react";
+import { auth } from "./../Firebase/Firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const createUserGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const userSignOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    createUserGoogle,
+    userLogin,
+    userSignOut,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Main from "./Main/Main";
+import router from "./Routes/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./Provider/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}>
+        <Main />
+        <Toaster />
+      </RouterProvider>
+    </QueryClientProvider>
+  </AuthProvider>
+);
